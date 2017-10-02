c55b5347725a53a667b16c99426e77764f1e7c12
Ahmet ÖZTÜRK <rawsludge@hotmail.com>
2017-10-02 22:31:48 +0300

    initial commit

---
+++ src/main/java/net/mobilim/NaviGateData/Entities/Product.java
@@ -0,0 +1,151 @@
+package net.mobilim.NaviGateData.Entities;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table( name = "PRODUCT")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "DEST_ID")
+    private Destination destination;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "EMBARK_PORT_ID")
+    private Port embarkPort;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "SHIP_ID")
+    private Ship ship;
+
+    @Column(name = "DURATION")
+    private Integer duration;
+
+    @Column(name = "SAILING_DATE")
+    private Date sailingDate;
+
+    @Column(name = "MAX_OCCUPANCY")
+    private Integer maxOccupancy;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "DEBARK_PORT_ID")
+    private Port debarkPort;
+
+    @Column(name = "SAILING_ID")
+    private String dailingID;
+
+    @Column(name = "CRUISE_LINE_CODE")
+    private String cruiseLineCode;
+
+    @Column(name = "INSERT_DATE")
+    private Date insertDate;
+
+    @Column(name = "LAST_UPDATE_DATE")
+    private Date lastUpdateDate;
+
+
+    public Product() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        id = id;
+    }
+
+    public Destination getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Destination destination) {
+        this.destination = destination;
+    }
+
+    public Port getEmbarkPort() {
+        return embarkPort;
+    }
+
+    public void setEmbarkPort(Port embarkPort) {
+        this.embarkPort = embarkPort;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Date getSailingDate() {
+        return sailingDate;
+    }
+
+    public void setSailingDate(Date sailingDate) {
+        this.sailingDate = sailingDate;
+    }
+
+    public Integer getMaxOccupancy() {
+        return maxOccupancy;
+    }
+
+    public void setMaxOccupancy(Integer maxOccupancy) {
+        this.maxOccupancy = maxOccupancy;
+    }
+
+    public Port getDebarkPort() {
+        return debarkPort;
+    }
+
+    public void setDebarkPort(Port debarkPort) {
+        this.debarkPort = debarkPort;
+    }
+
+    public String getDailingID() {
+        return dailingID;
+    }
+
+    public void setDailingID(String dailingID) {
+        this.dailingID = dailingID;
+    }
+
+    public String getCruiseLineCode() {
+        return cruiseLineCode;
+    }
+
+    public void setCruiseLineCode(String cruiseLineCode) {
+        this.cruiseLineCode = cruiseLineCode;
+    }
+
+    public Date getInsertDate() {
+        return insertDate;
+    }
+
+    public void setInsertDate(Date insertDate) {
+        this.insertDate = insertDate;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+}
---
+++ src/main/java/net/mobilim/NaviGateData/Entities/Ship.java
@@ -0,0 +1,45 @@
+package net.mobilim.NaviGateData.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "SHIP")
+public class Ship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "CODE")
+    private String code;
+
+    @Column(name = "NAME")
+    private String name;
+
+
+    public Ship(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
