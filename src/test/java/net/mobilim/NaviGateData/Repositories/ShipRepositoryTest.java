e1011423cafbfdc9228603fad452e36da2ce2cda
Ahmet ÖZTÜRK <rawsludge@hotmail.com>
2017-10-05 22:33:07 +0300

    just tested

--- src/test/java/net/mobilim/NaviGateData/Repositories/ShipRepositoryTest.java
+++ src/test/java/net/mobilim/NaviGateData/Repositories/ShipRepositoryTest.java
@@ -1,6 +1,5 @@
 package net.mobilim.NaviGateData.Repositories;
 
-import net.mobilim.NaviGateData.Entities.Port;
 import net.mobilim.NaviGateData.Entities.Ship;
 import org.junit.Test;
 import org.junit.runner.RunWith;
