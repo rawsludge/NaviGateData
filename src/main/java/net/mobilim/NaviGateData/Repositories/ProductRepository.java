a952d881763a43a8539985f4933f874e170125bf
Ahmet.Ozturk@cardtek.com <Ahmet.Ozturk@cardtek.com>
2017-10-05 09:25:42 +0300

    findBySailing method added to ProductRepository class

--- src/main/java/net/mobilim/NaviGateData/Repositories/ProductRepository.java
+++ src/main/java/net/mobilim/NaviGateData/Repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package net.mobilim.NaviGateData.Repositories;
 import net.mobilim.NaviGateData.Entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,4 +14,7 @@ import java.sql.Date;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query("SELECT p FROM Product p WHERE p.sailingDate BETWEEN :fromDate AND :toDate")
     Product findBySailingDate(Date fromDate, Date toDate);
+
+    @Query("SELECT p FROM Product p WHERE p.sailingID = :sailingID")
+    Product findBySailingID(@Param("sailingID") String sailingID);
 }
