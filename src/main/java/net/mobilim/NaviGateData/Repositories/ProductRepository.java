5efe095b4648099701d79cef795feadcceb10532
Ahmet ÖZTÜRK <rawsludge@hotmail.com>
2017-10-27 01:33:06 +0300

    Added paging support for findAvailableProducts method

--- src/main/java/net/mobilim/NaviGateData/Repositories/ProductRepository.java
+++ src/main/java/net/mobilim/NaviGateData/Repositories/ProductRepository.java
@@ -1,14 +1,15 @@
 package net.mobilim.NaviGateData.Repositories;
 
 import net.mobilim.NaviGateData.Entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
-import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
     @Query("SELECT p FROM Product p WHERE p.sailingDate BETWEEN :fromDate AND :toDate")
     Product findBySailingDate(Date fromDate, Date toDate);
 
@@ -16,10 +17,11 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     Product findBySailingID(@Param("sailingID") String sailingID);
 
     @Query("SELECT p FROM Product p WHERE p.sailingDate BETWEEN :fromDate AND :toDate AND p.duration BETWEEN :minDuration AND :maxDuration AND p.ship.code LIKE  :shipCode AND p.destination.code LIKE :destCode")
-    List<Product> findAvailableProducts(@Param("fromDate") Date fromDate,
+    Page<Product> findAvailableProducts(
+                                        @Param("fromDate") Date fromDate,
                                         @Param("toDate") Date toDate,
                                         @Param("minDuration") Integer minDuration,
                                         @Param("maxDuration") Integer maxDuration,
                                         @Param("shipCode") String shipCode,
-                                        @Param("destCode") String destCode);
+                                        @Param("destCode") String destCode, Pageable pageable);
 }
